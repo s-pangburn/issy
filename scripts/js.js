41057d9c31a3b20d9dd0a4882c32a2ced78362ee
Stephen Pangburn <stephen.pangburn.ii@gmail.com>
2018-11-11 18:14:40 -0800

    Set clock to standard time format

--- scripts/js.js
+++ scripts/js.js
@@ -11,12 +11,20 @@ function checkTime(i) {
   return i;
 }
 
+function setStandardTime(h) {
+  if (h > 12) return h - 12;
+  if (h === 0) return 12;
+}
+
 function time() {
   var today = new Date();
   var h = today.getHours();
   var m = today.getMinutes();
   var s = today.getSeconds();
   // add a zero in front of numbers<10
+  h = setStandardTime(h);
+  
+  h = checkTime(h);
   m = checkTime(m);
   s = checkTime(s);
   document.getElementById('time').innerHTML = h + ":" + m + ":" + s;
